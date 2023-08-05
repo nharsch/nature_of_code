4e7e4dade59e76601b5d4166d9ae08383266e4d8
nharsch <nigelharsch@gmail.com>
2023-08-05 15:45:04 -0700

    ch5 - mouseConstraint

--- nature-of-clojurescript/src/main/ch5.cljs
+++ nature-of-clojurescript/src/main/ch5.cljs
@@ -9,8 +9,8 @@
 (def canvas-id "ch5-canvas")
 (def width 400)
 (def height 400)
-(def cons-len 40)
-(def chain-len 8)
+(def cons-len 20)
+(def chain-len 5)
 
 ; stateful!
 (def engine)
@@ -32,6 +32,14 @@
   (set! world (.-world engine))
   ;; (set! ground (p/boundary 200 300 (- width  50) 20 0.1))
   ;; (.add m/Composite world (:body ground))
+  (let [canvas (.getElementById js/document "defaultCanvas0")  ; hacky but works, not sure how to grab onto quil canvas html el
+        canvas-mouse (.create m/Mouse canvas)
+        opts #js { "mouse" canvas-mouse }
+        mConstraint (.create m/MouseConstraint engine opts)]
+    (.add m/Composite world mConstraint)
+    ; retina fix
+    (set! (.-pixelRatio (.-mouse mConstraint)) (q/pixel-density))
+    )
   (set! state (atom {:chain [] :clicked? false}))
   (let [chain (let [xstart (/ width 2)]
                 (for [xoff (range chain-len)]
@@ -49,12 +57,10 @@
         (let [prev (:body (nth chain (- idx 1)))]
                                         ; create constraint connecting to previous
           (.add m/Composite world
-                (.create m/Constraint #js {
-                                           "bodyA" (:body (nth chain (- idx 1)))
+                (.create m/Constraint #js {"bodyA" (:body (nth chain (- idx 1)))
                                            "bodyB" (:body c)
                                            "length" cons-len
-                                           "stiffness" 0.4
-                                           })))))))
+                                           "stiffness" 0.1})))))))
 
 
 (defn draw []
