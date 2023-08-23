fdcc219ba3e49ee3d4a0a2ec1e334870294ee93f
nharsch <nigelharsch@gmail.com>
2023-08-22 23:48:35 -0700

    finished ch7

--- nature-of-clojurescript/src/main/ch7.cljs
+++ nature-of-clojurescript/src/main/ch7.cljs
@@ -4,10 +4,10 @@
             [util :as u]))
 
 (def canvas-id "ch7-canvas")
-(def width 500)
-(def height 1000)
+(def width 600)
+(def height 300)
 (def state (atom nil))
-(def res 100)
+(def res 200)
 
 
 (defn make-3d-array [ncols nrows]
@@ -109,7 +109,6 @@
 
 (defn gol-draw []
   (q/background 0)
-  ;; (q/frame-rate 10)
   (let [cells (:cells @state)
         cwidth (/ width res)
         rheight (/ height res)]
@@ -141,31 +140,36 @@
 (def rules
   {
    30 [0 1 1 1 1 0 0 0]
+   110 [0 1 1 0 1 1 1 0]
+   90 [0 1 0 1 1 0 1 0]
    })
 (rules 30)
+
+(defn seed-cells [len]
+  (-> (repeat len 0) vec (assoc (/ len 2) 1))
+  )
+(= (seed-cells 3) [0 1 0])
 
 
 (defn binary-args-to-int [args]
   (js/parseInt (apply str args) 2))
 (= 2 (binary-args-to-int [0 1 0]))
 
+; TODO: this could be faster with bitwise operation
 (defn neighborhood->next-cell [neighbors rule]
   (-> neighbors
       binary-args-to-int
       rule))
+
+
 (do
   (=  (neighborhood->next-cell [0 1 0] (rules 30)) 1)
   (=  (neighborhood->next-cell '(0 1 0) (rules 30)) 1)
   (=  (neighborhood->next-cell [0 0 1] (rules 30)) 1)
   (=  (neighborhood->next-cell [1 1 1] (rules 30)) 0))
 
-(defn seed-cells [len]
-  (-> (repeat len 0)
-      vec
-      (assoc (/ len 2) 1)))
-(= (seed-cells 3) [0 1 0])
 
-
+; TODO: this could be faster with bitwise operation
 (defn next-gen [cells rule]
   (vec (for [coords (partition 3 1 (range (+ 2 (count cells))))]
          (-> (concat [0] cells [0])     ; pad cells with 0 for edges
@@ -175,27 +179,53 @@
 (=  (next-gen [0 0 1 0 0] (rules 30)) [0 1 1 1 0])
 
 (defn ca-setup []
-  (reset! ca-state {:selected-rule (rules 30)
+  (reset! ca-state {:selected-rule 30
                     :generation 0
-                    :cells (seed-cells res)}))
+                    :cells (conj (vec (replicate (- res 1) (vec (replicate res 0)))) (seed-cells res))}))
+
 
 (defn ca-draw []
-  ;; (q/background 255)
-  (q/frame-rate 10)
-  (let [cells (:cells @ca-state)
-        gen (:generation @ca-state)
-        csize (/ width (count cells))]
-    (doseq [c (range (count cells))]
-      (q/no-stroke)
-      (q/fill (- 255  (* 255 (cells c))))
-      (q/rect (* c csize) (* gen csize) csize csize)))
-  (swap! ca-state update :generation inc)
-  (swap! ca-state update :cells #(next-gen % (:selected-rule @ca-state)))
-  )
+  (q/background 255)
+  ;; (q/frame-rate 20)
+  (let [csize (/ width (count (first (:cells @ca-state))))
+        visible-rows (/ height csize)
+        cells (:cells @ca-state)]
+    (doseq [[ri row] (map-indexed vector cells)]
+      (doseq [c (range (count row))]
+        (q/no-stroke)
+        (q/fill (- 255  (* 255 (row c))))
+        (q/rect (* c csize) (* ri csize) csize csize)))
+    (swap! ca-state update :generation inc)
+    (swap! ca-state update :cells #(subvec (conj % (next-gen
+                                              (last cells)
+                                              (rules (:selected-rule @ca-state))))
+                                           (if (< visible-rows (count cells))
+                                             (- (count cells) visible-rows)
+                                             0)))))
+
+(defn on-click [e]
+  (swap! ca-state assoc :selected-rule (rand-nth (keys rules))))
+
+(defn change-rule [n]
+  (swap! ca-state assoc :selected-rule n))
 
 (if (.getElementById js/document canvas-id)
   (do
     (u/create-div canvas-id "simple-ca")
+    ;; TODO: clean up
+    (let [b1 (.createElement js/document "button")
+          b2 (.createElement js/document "button")
+          b3 (.createElement js/document "button")
+          m (.getElementById js/document canvas-id)]
+      (set! (.-innerHTML b1) "rule 30")
+      (set! (.-onclick b1) #(change-rule 30))
+      (set! (.-innerHTML b2) "rule 90")
+      (set! (.-onclick b2) #(change-rule 90))
+      (set! (.-innerHTML b3) "rule 110")
+      (set! (.-onclick b3) #(change-rule 110))
+      (.appendChild m b1)
+      (.appendChild m b2)
+      (.appendChild m b3))
     (qs/defsketch fv
       :host "simple-ca"
       :title "simple-ca"
